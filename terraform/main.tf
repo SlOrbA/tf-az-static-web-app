4348ecd6000e9a46e74d1cd67f68968704c7e594
Lari Korpi <lari.korpi@tieto.com>
2021-10-13 10:38:55 +0300

    added muck Headless CMS

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,60 @@ resource "azurerm_static_site" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 }
+
+resource "azurerm_storage_account" "example" {
+  name                = "mockcms${random_string.uniquer.result}"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  account_replication_type = "LRS"
+  account_tier             = "Standard"  
+
+  static_website {
+    error_404_document = "index.html"
+    index_document     = "index.html"
+  }
+
+  blob_properties {
+    cors_rule {
+       allowed_headers    = [ "", ]
+       allowed_methods    = [ "GET", "HEAD", "OPTIONS", ]
+       allowed_origins    = [ "*", ]
+       exposed_headers    = [ "", ]
+       max_age_in_seconds = 10
+    }
+  }
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "cards"
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  source_content         = <<EOT
+     {
+	"cards" : [
+		{
+			"name": "card1",
+			"category" : "frontpage",
+			"content" : "this is content text 1st"
+		},
+		{
+			"name": "card2",
+			"category" : "frontpage",
+			"content" : "this is content text 2nd"
+		},
+		{
+			"name": "card3",
+			"category" : "frontpage",
+			"content" : "this is content text 3rd"
+		},
+				{
+			"name": "card4",
+			"category" : "frontpage",
+			"content" : "this is content text 4th"
+		}
+	]
+     }
+     EOT
+}
