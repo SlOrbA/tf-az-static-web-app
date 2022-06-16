98aad5b4f629a1811580c593cd9e0e781b6b3e4f
Lari Korpi <lari.korpi@tieto.com>
2022-06-16 09:44:25 +0300

    Lots of changes

--- demo-front/src/index.tsx
+++ demo-front/src/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import FirstComponent from './components/FirstComponent'
 ReactDOM.render(
-<div>
-<h1>Hello, Welcome to React and TypeScript</h1>
-</div>,
-  document.getElementById("root")
+    <div>
+      <h1>Hello, Welcome to React and TypeScript</h1>
+      <FirstComponent/>
+    </div>,
+    document.getElementById("root")
 );
