37a997f1eb09af6d43c35d8fee6f263acb669ebd
Lari Korpi <lari.korpi@tieto.com>
2022-06-16 10:23:46 +0300

    fix React build

--- demo-front/src/index.tsx
+++ demo-front/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import FirstComponent from './components/FirstComponent'
+import FirstComponent from './components/FirstComponent/FirstComponent'
 ReactDOM.render(
     <div>
       <h1>Hello, Welcome to React and TypeScript</h1>
