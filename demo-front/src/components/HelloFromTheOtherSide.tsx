b50c8e4e64122a32480d96b747d2645071adeb55
Lari Korpi <lari.korpi@tieto.com>
2022-06-16 13:26:04 +0300

    new demo-front version

---
+++ demo-front/src/components/HelloFromTheOtherSide.tsx
@@ -0,0 +1,25 @@
+import axios, {AxiosResponse} from 'axios'
+import { response } from 'express';
+function HelloFromTheOtherSide() {
+
+    var config = {
+        method: 'get',
+        url: 'https://icy-river-0d35d7e03.azurestaticapps.net/api/HelloFromTheOtherside?name=Me',
+        headers: { }
+    };
+
+    const HelloResponse = axios.get<String>(config.url)
+    .then(response => {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+    return(
+        <div>
+            <p>{JSON.stringify(HelloResponse)}</p>
+        </div>
+    );
+}
+export default HelloFromTheOtherSide;
